4234fd6785f98e7b68f8d1c1cde7ddf7113a6ba3
Gitika Kalal <gitikakalal21@gmail.com>
2025-05-05 22:06:00 -0700

    implement PUT /tasks/<task_id> routes

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -50,3 +50,23 @@ def validate_task(task_id):
         }
         abort(make_response(message, 404))
     return task
+
+@task_bp.put("/<task_id>")
+def update_task(task_id):
+
+    task = validate_task(task_id)
+
+    request_body = request.get_json()
+
+    try:
+        task.title = request_body["title"]
+        task.description = request_body["description"]
+    except KeyError as error:
+        message = {
+            "message": f"Missing '{error.args[0]}' attribute"
+        }
+        abort(make_response(message, 400))
+
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
